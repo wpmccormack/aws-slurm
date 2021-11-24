0154f1099b1db5f69509ab8d4c63ae0a0b71cb27
William McCormack <williammccormack@dhcp-10-29-106-4.dyn.mit.edu>
2021-11-23 19:39:43 -0500

    updating with security stuff now

--- startup-image-test-2.sh
+++ startup-image-test-2.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 echo "The script has 0 arguments"
+yum -y install wget
+yum -y install epel-release
+yum -y install emacs
 yum -y install https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest.noarch.rpm
 yum -y install cvmfs cvmfs-config-default cvmfs-auto-setup
-cvmfs_config setup
-echo "CVMFS_REPOSITORIES=cms.cern.ch,sft.cern.ch,grid.cern.ch,oasis.opensciencegrid.org" >> /etc/cvmfs/default.local
-echo "CVMFS_HTTP_PROXY=DIRECT" >> /etc/cvmfs/default.local
-cvmfs_config probe
+sudo cvmfs_config setup
+sudo echo "CVMFS_REPOSITORIES=cms.cern.ch,sft.cern.ch,grid.cern.ch,oasis.opensciencegrid.org" >> /etc/cvmfs/default.local
+sudo echo 'CVMFS_HTTP_PROXY="10.0.0.171:3128|10.0.0.96:3128|10.0.0.248:3128|10.0.0.25:3128|10.0.0.181:3128|10.0.0.7:3128|10.0.0.176:3128|10.0.0.196:3128"' >> /etc/cvmfs/default.local
+#sudo echo 'CVMFS_HTTP_PROXY="10.0.0.62:3128|10.0.0.143:3128|10.0.0.223:3128|10.0.0.65:3128|10.0.0.184:3128|10.0.0.40:3128|10.0.0.130:3128|10.0.0.149:3128"' >> /etc/cvmfs/default.local
+#sudo echo "CVMFS_HTTP_PROXY=DIRECT" >> /etc/cvmfs/default.local
+sudo cvmfs_config probe
 yum -y install per15
 yum -y install perl-Params-Validate
 rpm -ql perl-Params-Validate
@@ -15,3 +20,18 @@ cpan Test::Harness::Straps
 perl ./Build.PL
 export SCRAM_ARCH=slc7_amd64_gcc900
 source /cvmfs/cms.cern.ch/cmsset_default.sh
+pwd
+curr=${PWD}
+mkdir /home/centos/cms_path
+CMS_PATH=/home/centos/cms_path
+mkdir $CMS_PATH/SITECONF/
+mkdir $CMS_PATH/SITECONF/local
+mkdir $CMS_PATH/SITECONF/local/JobConfig
+mkdir $CMS_PATH/SITECONF/local/PhEDEx
+cd $CMS_PATH/SITECONF/local/JobConfig
+wget https://patrick-test-1.s3.amazonaws.com/site-local-config.xml
+cd
+cd $CMS_PATH/SITECONF/local/PhEDEx
+wget https://patrick-test-1.s3.amazonaws.com/storage.xml
+cd
+ll $CMS_PATH/SITECONF/local/JobConfig/site-local-config.xml
